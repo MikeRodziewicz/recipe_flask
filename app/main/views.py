d9bba46a4127bd88799ed9499305c69ec0af3caa
Mike.Rodziew <michal.rodziewicz1@gmail.com>
2021-09-22 21:45:54 +0200

    way too early for auth, working on forms and DB

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,26 @@
+from re import sub
 from flask import render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from . import main
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 @main.route('/', methods=['GET'])
 def index():
   return render_template('index.html')
 
 
-@main.route('/secret', methods=['GET'])
+@main.route('/secret', methods=['GET', 'POST'])
 def secret():
-  return render_template('secret.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data  = ''
+    return render_template('secret.html', form=form, name=name)
