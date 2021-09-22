0711f1e294fa581a68918ff0d6887574d3b94d70
Mike.Rodziew <michal.rodziewicz1@gmail.com>
2021-09-22 18:32:49 +0200

    working on user authentication

--- app/main/views.py
+++ app/main/views.py
@@ -4,3 +4,8 @@ from . import main
 @main.route('/', methods=['GET'])
 def index():
   return render_template('index.html')
+
+
+@main.route('/secret', methods=['GET'])
+def secret():
+  return render_template('secret.html')
