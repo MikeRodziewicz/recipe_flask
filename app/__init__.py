8a061437420a7118b3c84e590f16f12a462d861a
Mike.Rodziew <michal.rodziewicz1@gmail.com>
2021-09-23 21:23:30 +0200

    connected DB start working on models

--- app/__init__.py
+++ app/__init__.py
@@ -4,17 +4,23 @@ import os
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
 
 bootstrap = Bootstrap()
 moment = Moment()
+db = SQLAlchemy()
 
 def create_app():
+    basedir = os.path.abspath(os.path.dirname(__file__))
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'supersecretkey'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     bootstrap.init_app(app)
     moment.init_app(app)
-
+    db.init_app(app)
 
 # Each blueprint has to be registered here with the app itself, so the same goes for auth, but also for any other packages i have. 
     from .main import main as main_blueprint
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 from app import auth
-from flask import render_template
+from flask import render_template, session, url_for, redirect
 from .forms import LoginForm, RegistrationForm
 from . import auth
 
@@ -9,6 +9,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         pass
+        return redirect(url_for('main.index'))
     return render_template('auth/login.html', form=form)
 
 
@@ -17,4 +18,5 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         pass
+        return redirect(url_for('main.index'))
     return render_template('auth/register.html', form=form)
