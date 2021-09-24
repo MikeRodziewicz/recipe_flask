435dcf05baf460f08a1e5f1a8b1444b89bdd4d30
Mike.Rodziew <michal.rodziewicz1@gmail.com>
2021-09-24 21:23:51 +0200

    added two models with relationship

--- app/models.py
+++ app/models.py
@@ -1,12 +1,29 @@
 from enum import unique
-from operator import index
+from operator import contains, index
+from sqlalchemy.orm import backref
 from . import db
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, unique=True, index=True)
 
     def __repr__(self) -> str:
         return f'This is the user e-mail {self.email}'
+
+
+class Container(db.Model):
+    container_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, index=True)
+    capacity = db.Column(db.Integer)
+    ingredient = db.relationship('Ingredient', backref='container')
+
+
+class Ingredient(db.Model):
+    ingredient_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, index=True)
+    quantity = db.Column(db.Integer)
+    container_id = db.Column(db.Integer, db.ForeignKey('container.container_id'))
+
+
 
