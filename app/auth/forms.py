d03bffa09577bdf737a59642e1f5c3d6946773ca
Mike.Rodziew <michal.rodziewicz1@gmail.com>
2021-10-03 10:59:36 +0200

    registration done

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.fields.simple import PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo 
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError 
+from app.models import User
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -15,4 +16,9 @@ class RegistrationForm(FlaskForm):
     EqualTo('pass_2', message='Passwords do not match.')])
     pass_2 = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField('Register')
+
+    def check_if_user_already_exists(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('This email has already been used')
 
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,12 +1,12 @@
 from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
 from flask_login import login_user, logout_user, current_user
+from app import db
 from app.auth import auth
-from app.auth.forms import LoginForm
+from app.auth.forms import LoginForm, RegistrationForm
 from app.models import User
 
 
-# TODO login route register blueprint 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated: 
@@ -25,50 +25,25 @@ def login():
     return render_template('auth/login.html', form=form)
         
 
-#TODO logout route
 @auth.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('main.index'))
 
-# TODO registration route 
 
 
+#TODO register form to be created
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data)
+        user.set_password(form.pass_1.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('you are registered')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
 
-
-# #TODO login form to be created
-# @auth.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('main.index'))
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(email=form.email.data.lower()).first()
-#         if user is not None and user.verify_password(form.password.data):
-#             login_user(user)
-#             next = request.args.get('next')
-#             if next is None or not next.startswith('/'):
-#                 next = url_for('main.index')
-#             return redirect(next)
-#         flash('Invalid username or password.')
-#     return render_template('auth/login.html', form=form)
-
-
-# @auth.route('/logout', methods=['GET', 'POST'])
-# @login_required
-# def logout():
-#     logout_user()
-#     flash('you are no longer logged in')
-#     return redirect(url_for('main.index'))
-
-
-# #TODO register form to be created
-# @auth.route('/register', methods=['GET', 'POST'])
-# def register():
-#     form = RegistrationForm()
-#     if form.validate_on_submit():
-#         pass
-#         return redirect(url_for('main.index'))
-#     return render_template('auth/register.html', form=form)
-
-# #TODO add User profile page use user.html file for tha
